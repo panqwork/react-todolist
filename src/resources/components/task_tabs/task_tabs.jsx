fafc09ffebbf36d22cf167fb98d6a616dafe5c3e
Dmitriy <panqwork@gmail.com>
2020-06-26 18:18:12 +0500

    Style on active tab added

--- src/resources/components/task_tabs/task_tabs.jsx
+++ src/resources/components/task_tabs/task_tabs.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import {Link} from 'react-router-dom';
+import s from './style.module.scss'
 
-export const TaskTabs = () => {
+import {Link, withRouter} from 'react-router-dom';
+
+const TaskTabsComponent = (props) => {
+	let activeTab = (props.location.pathname).substring(1) || 'all';
 	return(
-		<>
+		<div className={[s.tabs, s[activeTab]].join(' ')}>
 			<Link to={'/'}>All</Link>
 			<Link to={'/active'}>Active</Link>
 			<Link to={'/completed'}>Completed</Link>
-		</>
+		</div>
 	)
 }
+
+export const TaskTabs = withRouter(TaskTabsComponent)
