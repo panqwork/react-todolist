4928465863cd94d39918d3d800c47219c38defb7
Dmitriy <panqwork@gmail.com>
2020-07-21 18:47:32 +0500

    Updates

---
+++ precache-manifest.9f543fd1208c152057715fa300299cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf4ddc7b5257c7f331bbfc994e9885a",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "31e84fcf436158acaf5a",
+    "url": "/react-todolist/static/css/main.47223d56.chunk.css"
+  },
+  {
+    "revision": "521fd51b8e9563f7ac7b",
+    "url": "/react-todolist/static/js/2.57d0a95f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist/static/js/2.57d0a95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e84fcf436158acaf5a",
+    "url": "/react-todolist/static/js/main.17fa54e6.chunk.js"
+  },
+  {
+    "revision": "5854e264c0d596de1540",
+    "url": "/react-todolist/static/js/runtime-main.a9924442.js"
+  }
+]);
